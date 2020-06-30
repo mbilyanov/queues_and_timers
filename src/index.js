3d3fccf5b3a7a4a0b438d26f093da3ea6e04b950
mbilyanov <milen.bilyanov@gmail.com>
2020-06-30 19:30:10 +0100

    First working prototype of the queue handler with retries.

--- src/index.js
+++ src/index.js
@@ -53,8 +53,18 @@ class RequestHandler extends Queue {
     #isBusy = false;
     #callCounter = 0;
 
+    // This is the dynamic container that will store things like retry counters etc.
+    //      fields:5m:XRP/EUR
+    #fields = {};
+
+    // This is the final object that will be returned once the whole quueue is
+    // consumed. The data structure template is described below:
+    //      consumed:states:hasErrors:true
+    //      consumed:requests:5m:failed_tasks:XRP/EUR
+    #consumed = {'states': {'hasErrors': false}, 'requests': {}};
+
     constructor(name=null) {
-        super(20);
+        super(100);
 
         this.name = name == null ? 'printer_' + Math.floor(Math.random() * 10000) : 'printer_' + name;
         this.id = Math.floor(Math.random() * 10000);
@@ -64,55 +74,112 @@ class RequestHandler extends Queue {
     async processQueue() {
         this.#isBusy = true;
         let task;
+        let retryCounter;
 
-        const result = {
-            'errors': false,
-            'message': null
-        };
         const queueItem = this.dequeue();
+        const asset = queueItem.asset;
+        const timeframe = queueItem.timeframe;
+        const specs = queueItem.specs;
 
-        // We are passing in an object with a data and a function side.
-        // We do not need the function for now.
-        //const docCall = queueItem.fn;
-        const docSignal = queueItem.sg;
+        // Populate the 'consumed' container.
+        // (example) consumed:requests:5m:failed_tasks:['XRP/EUR', 'BTX/USD']
+        if(!(timeframe in this.#consumed.requests)){
+            this.#consumed.requests[timeframe] = { 'failed_tasks': []};
+        }
+
+        // Populate the 'fields' container.
+        // (example) fields:5m
+        if(!(timeframe in this.#fields)){
+            this.#fields[timeframe] = {};
+        }
+
+        // fields:5m:XRP/EUR:retry_counter:2
+        if(!(asset in this.#fields[timeframe])){
+            this.#fields[timeframe][asset] = {'retry_counter': specs.maxRetries};
+
+            // The retry_counter is initialized here.
+            retryCounter = specs.maxRetries;
+        }else{
+            // If already present, we use the dynamic value NOT the value that
+            // comes through the specs.
+            retryCounter = this.#fields[timeframe][asset]['retry_counter'];
+        }
 
         try{
-            task = await queryMockTickers(queueItem.sg);
-            console.log(`Ticker data received. [${queueItem.sg}], __QUEUE_SIZE__: ${this.size}`);
-            console.log('__db__WRITE: monogodb');
+            // Actual ticker fetcher will support different time frames.
+            task = await queryMockTickers(asset);
+            //console.log(`SUCCESS: Ticker data received. [${asset}], [${timeframe}], __QUEUE_SIZE__: ${this.size}`);
+            //console.log('__db__WRITE: monogodb');
+            //console.log(JSON.stringify(task));
         }catch(e){
-            task = 'failed';
-            console.log(`Ticker data failed. [${queueItem.sg}], __QUEUE_SIZE__: ${this.size}`);
-            console.log('continue ...');
-            result['errors'] = true;
+            task = null;
+            //console.log(`FAIL: Ticker data failed. [${asset}], [${timeframe}], __QUEUE_SIZE__: ${this.size}`);
+
+            if(retryCounter != 0){
+                console.log(`retry ... for [${asset}] ... <${retryCounter}>`);
+                retryCounter -= 1;
+                this.#fields[timeframe][asset]['retry_counter'] = retryCounter;
+
+                // The next value of the retry counter will be pulled from the
+                // 'fields' container.
+                console.log(`updated _retryCounter ... <${retryCounter}>`);
+                this.submit(asset, timeframe, specs);
+            }else{
+                console.log(`Retry limit reached after ${specs.maxRetries} retries. Aborting any pending requests for [${asset}, ${timeframe}].`);
+                this.#consumed.requests[timeframe]['failed_tasks'].push(asset);
+
+                if(!this.#consumed.states.hasErrors){
+                    this.#consumed.states.hasErrors = true;
+                }
+            }
         }
         // Queue is consumed, we are done.
         if (this.isEmpty) {
             console.log('... done ...');
             this.#isBusy = false;
 
-            // Pack the last message.
-            result['message'] = task;
-            return result;
+            return this.#consumed;
         } else {
             console.log('... next ...');
             return await this.processQueue();
         }
     };
 
-    submit = (doc) => {
+    submit = (asset, timeframe, specs) => {
         if (this.isFull) {
-            console.log("Printer is full");
+            console.log('ERROR: Queue is full!');
         } else {
             let current_size;
-            current_size = this.enqueue({'fn': 'BANANA', 'sg': doc});
-            console.log(`__TASK__: ${doc} queue submission executed, __QUEUE_SIZE__: ${current_size}`);
+            current_size = this.enqueue({'asset': asset, 'timeframe': timeframe, 'specs': specs});
+            // console.log(`__TASK__: ${asset}, ${timeframe}, ${specs}, queue submission executed, __QUEUE_SIZE__: ${current_size}`);
         }
     };
 }
 // }}}1
 
 // RequestBundle {{{1
+//
+// RequestBundle.submit('5m', '20');
+// RequestBundle.submit('15m', '40');
+// RequestBundle.submit('1h', '60');
+// RequestBundle.submit('4h', '80');
+// RequestBundle.submit('1d', '100');
+//
+// every request will check the current level and elevate that if it is lower
+// than the target. For example, when the '5m' task accesses the contaoiner, it
+// will set the level to 20, later, when the '15m' task modifies the container,
+// it will check and find 20 as the current level, this will be elevated to 40,
+// so on and so on.
+//
+// The last taks that goes in there, will trigger the consumption of all queue
+// containers.
+//
+// We need to make sure that the invervals run sequentially, 5m, 15m, 1h etc.
+// or we will have to handle that in the container.
+//
+// We should be able to the intervals run sequentially.
+//
+// //
 class RequestBundle extends Queue {
     #printers = [
         new RequestHandler('5m'),
@@ -150,29 +217,92 @@ class RequestBundle extends Queue {
 
 /* The basic bits that are working. */
 
-const asset_list = ['BTC/EUR', 'ETH/EUR', 'ZEX/EUR', 'LTC/EUR', 'XMR/EUR', 'DASH/EUR', 'EOC/EUR', 'ETC/EUR', 'XLM/EUR', 'XRP/EUR', 'BANANA/APPLE', 'ABC/XYZ', 'FGH/TRY'];
+//const asset_list = ['BTC/EUR', 'ETH/EUR', 'ZEC/EUR', 'LTC/EUR', 'XMR/EUR', 'DASH/EUR', 'EOS/EUR', 'ETC/EUR', 'XLM/EUR', 'XRP/EUR', 'BTC/USD', 'ETH/USD', 'ZEC/USD', 'LTC/USD', 'XMR/USD', 'DASH/USD', 'EOS/USD', 'ETC/USD', 'XLM/USD', 'XRP/USD'];
+const asset_list = ['BTS/EUR', 'ETH/EUR'];
 
-const myPrinter_5m = new RequestHandler('5m');
+const assetRequester = new RequestHandler('asset_requester');
 
+// Initial submissions.
 try {
-asset_list
-    .forEach((n) => {
-        myPrinter_5m.submit(n);
-    });
+    console.log('Submittin to group [5m]');
+    asset_list
+        .forEach((n) => {
+            assetRequester.submit(n, '5m', {maxRetries: 1});
+        });
 }catch(e){
-    console.log('CRITICAL_FAILUER: Submission failed.');
+    console.log('CRITICAL_FAILURE: Submission failed for group [5m].');
 }
 
-myPrinter_5m.print();
+assetRequester.print();
 
+// ASYNC task consumer starts working immediately.
 (async () => {
     try{
-        let response = await myPrinter_5m.processQueue();
+        console.log('__LOCK_INTERVAL__');
+        let response = await assetRequester.processQueue();
         console.log(JSON.stringify(response));
+        console.log('__UNLOCK_INTERVAL__');
     }catch(e){
-        console.log(`xxx ${e} xxx`);
-    }
+        console.log(`CRITICAL_FAILURE: ${e}`);
+        console.log('__UNLOCK_INTERVAL__');
+    };
 })();
+
+// Test async sequential submission (15m).
+sleep(1000)
+    .then((message) => {
+        console.log('Submittin to group [15m]');
+        asset_list
+            .forEach((n) => {
+                assetRequester.submit(n, '15m', {maxRetries: 2});
+            })
+    })
+    .catch((err) => {
+        console.log(`CRITICAL_FAILURE: Submission failed for group [15M].\n${err}`);
+    })
+    .finally(() => { assetRequester.print() });
+
+// Test async sequential submission (1h).
+sleep(1000)
+    .then((message) => {
+        console.log('Submittin to group [1h]');
+        asset_list
+            .forEach((n) => {
+                assetRequester.submit(n, '1h', {maxRetries: 3});
+            })
+    })
+    .catch((err) => {
+        console.log(`CRITICAL_FAILURE: Submission failed for group [1h].\n${err}`);
+    })
+    .finally(() => { assetRequester.print() });
+
+// Test async sequential submission (4h).
+sleep(1000)
+    .then((message) => {
+        console.log('Submittin to group [4h]');
+        asset_list
+            .forEach((n) => {
+                assetRequester.submit(n, '4h', {maxRetries: 4});
+            })
+    })
+    .catch((err) => {
+        console.log(`CRITICAL_FAILURE: Submission failed for group [4h].\n${err}`);
+    })
+    .finally(() => { assetRequester.print() });
+
+// Test async sequential submission (1d).
+sleep(1000)
+    .then((message) => {
+        console.log('Submittin to group [1d]');
+        asset_list
+            .forEach((n) => {
+                assetRequester.submit(n, '1d', {maxRetries: 5});
+            })
+    })
+    .catch((err) => {
+        console.log(`CRITICAL_FAILURE: Submission failed for group [1d].\n${err}`);
+    })
+    .finally(() => { assetRequester.print() });
 
 /* Some old experiements */
 
